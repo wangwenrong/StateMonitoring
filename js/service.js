62c9ff4179842279150e8a587346402f6ccb14e8
王文荣 <wangwenrong@autoiots.com>
2018-04-12 18:04:07 +0800

    新增模板

--- js/service.js
+++ js/service.js
@@ -1,31 +1,34 @@
 var BASEURL = 'http://gis.cloud.rtzltech.cn:8010/monitoringserver/';
+project: 'http://app.test.cloud.rtzltech.cn/monitoringserver/',
 
 function getData(url, params, callback, errorCallBack) {
     $.ajax({
         type: 'POST',
-        data: params,
         url: BASEURL + url,
-        dataType: json,
+        data: params,
+        dataType: 'json',
         timeout: 30000,
         success: function (data) {
-            plus.nativeUI.closeWaiting();
             if (data && '99' == data.retcode) {
                 alert(data.retmsg);
                 return;
             }
             return callback(data);
         },
-        error: function (xhr, type, errorThrown) {
+        error: function (error) {
             if (errorCallBack) {
                 if (error.message === 'Network Error') {
                     alert('网络错误');
                 } else {
                     alert(error.message);
                 }
-                errorCallBack(error);
+                return errorCallBack(error);
             }
         }
     })
+}
+function login(params, callback) {
+    getData('login', params, callback);
 }
 function getmap(params, callback, errorCallBack) {
     getData('track/posDevDataQuery.action', params, callback, errorCallBack);
